97bfe8470aec3cfa593ccba49d12c379bbeac5b0
Yusuke-AUU <yusuke.tanaka@auuonline.com>
2025-04-19 13:28:59 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -108,14 +108,22 @@ function submitForm() {
     email: document.getElementById("email").value
   };
 
-fetch("https://script.google.com/macros/s/AKfycbzoZXeSOZnyOKMYjjSaoQYx_ElLCwKyImjjfh_6JWRbpgboejld2WHdtbS9FHaRpZdT/exec", {
+  const formData = new URLSearchParams();
+  for (const key in payload) {
+    formData.append(key, payload[key]);
+  }
+
+  fetch("https://script.google.com/macros/s/AKfycbwdf6QyNMTEpyaaPy284rMnPjZKDg-P3X2gapTglvjdiemoXwVzc1lzcfhmhaUPzcZc/exec", {
     method: "POST",
-    
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload)
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: formData.toString()
   });
 
-  typeMessage("ありがとうございます！内容を確認し、担当よりご連絡いたします。\n私たちは、貴社の益々の発展を応援しております！\n引き続きよろしくお願いいたします。", () => {
+  typeMessage("ありがとうございます！内容を確認し、担当よりご連絡いたします。
+私たちは、貴社の益々の発展を応援しております！
+引き続きよろしくお願いいたします。", () => {
     const restart = document.createElement("button");
     restart.textContent = "🔁 もう一度相談する";
     restart.className = "restart-button";
